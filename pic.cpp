e5b64fdd6c01f8ded1640f67cf15e5cd02e81b61
Stepan Roucka <stepan@roucka.eu>
2009-05-31 17:06:48 +0200

    Reset particle density on initialization
    This prevents from nans propagating in
    nonselfconsistent simulation with t_equilibrium <= t_print

--- pic.cpp
+++ pic.cpp
@@ -109,6 +109,10 @@ class Pic
 	    {
 		field.boundary_solve();
 		field.reset();
+		for(int i=0; i<NTYPES; i++) if(is_particle[i] && species_list[i] != NULL)
+		{
+		    species_list[i]->rho.reset();
+		}
 	    }
 	};
 	~Pic()
