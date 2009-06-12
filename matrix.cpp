9d185e16f45896cbdfaa1e29487549ffb7631085
Stepan Roucka <stepan@roucka.eu>
2009-06-12 22:55:29 +0200

    Implement 3D array

--- matrix.cpp
+++ matrix.cpp
@@ -1,7 +1,8 @@
 #ifndef MATRIX_H
 #define MATRIX_H
 
-#include <exception>
+#include <cstdlib>
+#include <stdexcept>
 
 template<class T>
 class Matrix
@@ -64,5 +65,93 @@ class Matrix
 		for(int l=0; l<lmax; l++)
 		    data[j][l] = 0;
 	}
+};
+
+template<class T>
+class Array3D
+{
+    public:
+        int imax, jmax,kmax;
+        T ***data;
+        Array3D() : imax(0), jmax(0), kmax(0), data(NULL) {}
+        Array3D(int _imax, int _jmax, int _kmax) : imax(_imax), jmax(_jmax), kmax(_kmax)
+        {
+
+            data = new T** [imax];
+            data[0] = new T* [imax*jmax];
+            data[0][0] = new T [imax*jmax*kmax];
+
+            for(int i=1; i<imax; i++)
+                data[i] = data[i-1] + jmax;
+
+            for(int i=0; i<imax; i++)
+            {
+                for(int j=0; j<jmax; j++)
+                {
+                    if(i==0 && j==0) continue;
+                    data[i][j] = data[i][j-1] + kmax;
+                }
+            }
+        }
+        void resize(int _imax, int _jmax, int _kmax)
+        {
+            this->~Array3D();
+
+            imax = _imax;
+            jmax = _jmax;
+            kmax = _kmax;
+            data = new T** [imax];
+            data[0] = new T* [imax*jmax];
+            data[0][0] = new T [imax*jmax*kmax];
+
+            for(int i=1; i<imax; i++)
+                data[i] = data[i-1] + jmax;
+
+            for(int i=0; i<imax; i++)
+            {
+                for(int j=0; j<jmax; j++)
+                {
+                    if(i==0 && j==0) continue;
+                    data[i][j] = data[i][j-1] + kmax;
+                }
+            }
+        }
+        ~Array3D()
+        {
+            if(data != NULL)
+            {
+                delete [] data[0][0];
+                delete [] data[0];
+                delete [] data;
+            }
+        }
+        T ** & operator [] (int i) const
+        {
+            return data[i];
+        }
+        void add(Array3D const &array)
+        {
+            if(imax != array.imax || jmax != array.jmax || kmax != array.kmax)
+                throw std::runtime_error("Array3D.add(): array sizes don't match\n");
+
+            for(int i=0; i<imax; i++)
+                for(int j=0; j<jmax; j++)
+                    for(int k=0; k<kmax; k++)
+                        data[i][j][k] += array[i][j][k];
+        }
+        void multiply(double x)
+        {
+            for(int i=0; i<imax; i++)
+                for(int j=0; j<jmax; j++)
+                    for(int k=0; k<kmax; k++)
+                        data[i][j][k] *= x;
+        }
+        void reset()
+        {
+            for(int i=0; i<imax; i++)
+                for(int j=0; j<jmax; j++)
+                    for(int k=0; k<kmax; k++)
+                        data[i][j][k] = 0;
+        }
 };
 #endif
