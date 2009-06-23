893cf8ca0bdcd98c66f4bb4cf65445b4b4df8e37
Stepan Roucka <stepan@roucka.eu>
2009-06-23 21:00:49 +0200

    Implement 3D Poisson solver.
    UMFPACK based Poisson equation solver was implemented.
    Along with this, I added a simple infrastructure for
    efficient modelling of complicated electrode geometries.

--- fields3d.cpp
+++ fields3d.cpp
@@ -2,8 +2,11 @@
 #define FIELD3D_H
 
 #include "matrix.cpp"
+#include "param.cpp"
+#include <suitesparse/umfpack.h>
 #include <iostream>
 #include <fstream>
+#include <vector>
 using namespace std;
 
 class Field3D : public Array3D<double>
@@ -13,6 +16,10 @@ class Field3D : public Array3D<double>
             Array3D<double>(x_sampl, y_sampl, z_sampl), xmin(0), ymin(0), zmin(0),
             idx(1.0/dx), idy(1.0/dy), idz(1.0/dz) {};
 
+        Field3D(Param &param) :
+            Array3D<double>(param.x_sampl, param.y_sampl, param.z_sampl), xmin(0), ymin(0), zmin(0),
+            idx(param.idx), idy(param.idy), idz(param.idz) {};
+
         inline void accumulate(double charge, double x, double y, double z);
         inline double interpolate(double x, double y, double z);
         void print( ostream & out = cout , double factor = 1.0);
@@ -21,6 +28,196 @@ class Field3D : public Array3D<double>
     private:
         double idx, idy, idz;
         double xmin, ymin, zmin;
+};
+
+class Electrode
+{
+    protected:
+        signed char id;
+    public:
+        Electrode(int i, double _voltage = 1.0) : id(i), voltage(_voltage) {};
+        double voltage;
+        virtual void set_mask(Array3D<signed char> &mask) = 0;
+        void set_voltage(Array3D<double> &voltage_array, Array3D<signed char> &mask)
+        {
+            for(int i = 0; i < voltage_array.imax*voltage_array.jmax*voltage_array.kmax; i++)
+                if(mask[0][0][i] == id)
+                    voltage_array[0][0][i] = voltage;
+        }
+};
+
+class Quadrupole : public Electrode
+{
+    public:
+        Quadrupole(int i) : Electrode(i) {};
+        void set_mask(Array3D<signed char> &mask)
+        {
+            mask[mask.imax/2][mask.jmax/2][mask.kmax/2] = id;
+            // to be implemented :)
+        };
+};
+
+enum {FREE, FIXED, BOUNDARY};
+
+class Geometry
+{
+    public:
+        double x_sampl, y_sampl, z_sampl;
+        Array3D<signed char> mask;
+        vector<Electrode*> electrodes;
+        vector<Array3D<double>*> potentials;
+        Field3D voltage;
+        Geometry(Param &param) : x_sampl(param.x_sampl), y_sampl(param.y_sampl),
+            z_sampl(param.z_sampl), mask(param.x_sampl, param.y_sampl, param.z_sampl),
+            electrodes(0), voltage(param)
+        {
+
+            //set zero Dirichlet boundary condition
+            for(int i = 0; i < param.x_sampl; i++)
+                for(int j = 0; j < param.y_sampl; j++)
+                    for(int k = 0; k < param.z_sampl; k++)
+                        if( i==0 || i==param.x_sampl-1 || j==0 || j==param.y_sampl-1 || k==0 || k==param.z_sampl)
+                            mask[i][j][k] = FIXED;
+                        else
+                            mask[i][j][k] = FREE;
+
+            electrodes.push_back(new Quadrupole(-1));
+            electrodes[0]->set_mask(mask);
+            electrodes[0]->set_voltage(voltage, mask);
+
+        }
+};
+
+class Solver
+{
+    private:
+        int *Ap;
+        int *Ai;
+        double *Ax;
+        void *Symbolic, *Numeric ;
+        Param &param;
+        Geometry &geometry;
+        double x_sampl, y_sampl, z_sampl;
+
+    public:
+        Solver(Geometry & _geometry, Param & _param) : param(_param), geometry(_geometry),
+            x_sampl(geometry.x_sampl), y_sampl(geometry.y_sampl), z_sampl(geometry.z_sampl)
+        {
+            int n = x_sampl * y_sampl * z_sampl;
+            Ap = new int [n+1];
+            Ai = new int [n*7]; //upper limit for 7-point diff scheme
+            Ax = new double [n*7];
+
+
+            int l = 0, m;
+            Ap[0] = 0;
+            for(int i=0; i<x_sampl; i++)
+                for(int j=0; j<y_sampl; j++)
+                    for(int k=0; k<z_sampl; k++)
+                    {
+                        m = y_sampl*z_sampl*i + z_sampl*j + k;
+                        if(geometry.mask[i][j][k] == FIXED || geometry.mask[i][j][k] < 0)
+                            //XXX is the FIXED still needed or is it replaced by electrode id?
+                        {
+                            Ax[l] = 1;
+                            Ai[l] = m;
+                            Ap[m+1] = Ap[m]+1;
+                            l++;
+                        }
+                        else
+                        {
+                            Ax[l] = Ax[l+1] = Ax[l+2] = 1.0;
+                            Ax[l+4] = Ax[l+5] = Ax[l+6] = 1.0;
+                            Ax[l+3] = -6;
+                            Ai[l] = m-y_sampl*z_sampl;
+                            Ai[l+1] = m-z_sampl;
+                            Ai[l+2] = m-1;
+                            Ai[l+3] = m;
+                            Ai[l+4] = m+1;
+                            Ai[l+5] = m+z_sampl;
+                            Ai[l+6] = m+y_sampl*z_sampl;
+                            Ap[m+1] = Ap[m]+7;
+                            l += 7;
+                        }
+                    }
+            (void) umfpack_di_symbolic (n, n, Ap, Ai, Ax, &Symbolic, NULL, NULL) ;
+            (void) umfpack_di_numeric (Ap, Ai, Ax, Symbolic, &Numeric, NULL, NULL) ;
+            umfpack_di_free_symbolic (&Symbolic) ;
+        };
+
+        void solve(Array3D<double> &u, const Array3D<double> &voltage, Array3D<double> &rho)
+        {
+            for(int i=0; i<x_sampl; i++)
+                for(int j=0; j<y_sampl; j++)
+                    for(int k=0; k<z_sampl; k++)
+                    {
+                        if(geometry.mask[i][j][k] == FIXED || geometry.mask[i][j][k] < 0)
+                            rho[i][j][k] = voltage[i][j][k];
+                        else
+                            rho[i][j][k] *= -param.macroparticle_factor/param.eps_0;
+                    }
+            (void) umfpack_di_solve (UMFPACK_At, Ap, Ai, Ax, u[0][0], rho[0][0], Numeric, NULL, NULL) ;
+        }
+
+        void solve_laplace(Array3D<double> &u, const Array3D<double> &voltage)
+            // solver for Laplace equation (zero RHS), it assumes that voltage is set to zero in free space
+        {
+            (void) umfpack_di_solve (UMFPACK_At, Ap, Ai, Ax, u[0][0], voltage[0][0], Numeric, NULL, NULL) ;
+        }
+};
+
+class ElMag3D
+{
+    public:
+        Field3D u, rho, voltage;
+        ElMag3D(Param &param) : potentials(0), geometry(param), solver(geometry, param),
+            u(param), rho(param), voltage(param), multielectrode(false)
+        {
+            //do we have multiple independent sets of electrodes in
+            //nonselfconsistent simulation
+            if(geometry.electrodes.size() > 1 && !param.selfconsistent)
+            {
+                for(int i=0; i<geometry.electrodes.size(); i++)
+                    potentials.push_back(new Field3D(param));
+                multielectrode = true;
+            }
+            rho.reset();
+        }
+        void solve()
+        {
+            if(multielectrode)
+            {
+
+                for(int i=0; i<geometry.electrodes.size(); i++)
+                {
+                    voltage.reset();
+                    geometry.electrodes[i]->set_voltage(voltage, geometry.mask);
+                    solver.solve_laplace(*(potentials[i]), voltage);
+                }
+            }
+            else
+            {
+                voltage.reset();
+                for(int i=0; i<geometry.electrodes.size(); i++)
+                    geometry.electrodes[i]->set_voltage(voltage, geometry.mask);
+                solver.solve(u, voltage, rho);
+            }
+        }
+        void potential_sum()
+            //TODO do a weighted sum of solutions for single electrodes,
+            //for now it just performs sum
+        {
+            u.reset();
+            for(int i=0; i<potentials.size(); i++)
+                u.add(*(potentials[i]));
+        }
+    private:
+        vector<Field3D*> potentials;
+
+        Geometry geometry;
+        Solver solver;
+        bool multielectrode;
+
 };
 
 inline void Field3D::accumulate(double charge, double x, double y, double z)
