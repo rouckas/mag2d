f672aa7c194fd770642dba5f2d5d63763f26ed63
Stepan Roucka <stepan@roucka.eu>
2011-10-28 12:36:36 +0200

    Fix t_timer.reset() and improve t_timer interface

--- src/timer.hpp
+++ src/timer.hpp
@@ -7,11 +7,11 @@ class t_timer
 {
 
     struct rusage usage2,usage1;
-    double time;
+    double cpu_time;
     double real_time;
     timeval tval1, tval2;
     public:
-    t_timer() : time(0), real_time(0) {} ;
+    t_timer() : cpu_time(0), real_time(0) {} ;
     void start()
     {
         getrusage(RUSAGE_SELF,&usage1);
@@ -20,16 +20,19 @@ class t_timer
     void stop() 
     { 
 	getrusage(RUSAGE_SELF,&usage2); 
-	time += (double)(usage2.ru_utime.tv_sec-usage1.ru_utime.tv_sec);
-        time += (double)(usage2.ru_utime.tv_usec-usage1.ru_utime.tv_usec)*1e-6;
+        cpu_time += (double)(usage2.ru_utime.tv_sec-usage1.ru_utime.tv_sec);
+        cpu_time += (double)(usage2.ru_utime.tv_usec-usage1.ru_utime.tv_usec)*1e-6;
 
         gettimeofday(&tval2, NULL);
         real_time += (double)(tval2.tv_sec-tval1.tv_sec);
         real_time += (double)(tval2.tv_usec-tval1.tv_usec)*1e-6;
     }
-    double get_cpu_time() { return time; };
+    double get_cpu_time() { return cpu_time; };
     double get_real_time() { return real_time; };
-    void reset() { time=0;};
+    void reset() {
+        cpu_time=0;
+        real_time=0;
+    };
 
 };
 #endif
